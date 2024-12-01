dff262be1d14532781ab8b2800d6215f745609ab
skrlssn <4572326@gmail.com>
2024-12-01 17:39:12 +0100

    setup repo

---
+++ day01/Program.cs
@@ -0,0 +1,15 @@
+﻿using aoc;
+
+var input = File.ReadLines("input.txt").Select(int.Parse);
+
+var part = Environment.GetEnvironmentVariable("part");
+
+var solution = part switch
+{
+    "part1" => Puzzle.GetSolutionPart1(input),
+    "part2" => Puzzle.GetSolutionPart2(input),
+    _ => throw new ArgumentOutOfRangeException(nameof(part), $"Unexpected {nameof(part)} value: '{part}'")
+};
+
+Console.WriteLine("C#");
+Console.WriteLine(solution);
